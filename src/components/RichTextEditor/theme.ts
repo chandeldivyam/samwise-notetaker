141515227e4f7ccbb9e8a469106276767af0a398
Divyam Chandel <chandeldivyam@gmail.com>
2025-01-11 15:03:57 +0530

    ol and ul ux css corrections

--- src/components/RichTextEditor/theme.ts
+++ src/components/RichTextEditor/theme.ts
@@ -18,6 +18,23 @@ const ExampleTheme = {
 		strikethrough: 'editor-text-strikethrough',
 		underlineStrikethrough: 'editor-text-underlineStrikethrough',
 	},
+	list: {
+		checklist: 'PlaygroundEditorTheme__checklist',
+		listitem: 'PlaygroundEditorTheme__listItem',
+		listitemChecked: 'PlaygroundEditorTheme__listItemChecked',
+		listitemUnchecked: 'PlaygroundEditorTheme__listItemUnchecked',
+		nested: {
+			listitem: 'PlaygroundEditorTheme__nestedListItem',
+		},
+		olDepth: [
+			'PlaygroundEditorTheme__ol1',
+			'PlaygroundEditorTheme__ol2',
+			'PlaygroundEditorTheme__ol3',
+			'PlaygroundEditorTheme__ol4',
+			'PlaygroundEditorTheme__ol5',
+		],
+		ul: 'PlaygroundEditorTheme__ul',
+	},
 };
 
 export default ExampleTheme;
